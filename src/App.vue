bd94ebb00fc7fa5a8c59e4f30bcc3f2b402dfa61
William Marques <wmarques.pro@gmail.com>
2022-11-26 21:58:22 +0100

    add spotify login

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,33 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import SearchBar from "./components/SearchBar.vue";
-import {authenticate} from "./api/SpotifyApi";
+import {getToken, login} from "./api/SpotifyApi";
 import {ref} from "vue";
 import {AlbumSearch} from "./models/AlbumSearch";
 import TierList from "./components/TierList.vue";
 
-authenticate();
 
-const currentAlbum = ref<AlbumSearch>()
+let params = new URLSearchParams(window.location.search);
+let code = params.get('code');
+
+
+if (code) {
+  params.delete('code');
+  getToken(code).then(c => console.log(c));
+}
+
+const currentAlbum = ref<AlbumSearch>();
+
+function doLogin() {
+  login();
+}
 
 </script>
 
 
 <template>
   <h1 class="text-3xl bg-black text-white px-4 py-2">Tier List Album</h1>
+  <button @click="doLogin">Login</button>
   <div class="container mx-auto mt-4">
     <SearchBar @albumSelected="e => currentAlbum = e" class="w-96 my-4"/>
     <TierList v-if="currentAlbum" :album="currentAlbum"/>
--- src/api/SpotifyApi.ts
+++ src/api/SpotifyApi.ts
@@ -1,36 +1,53 @@
-import axios from "axios";
+import axios, {AxiosInstance} from "axios";
+import {AlbumSearch} from "../models/AlbumSearch";
+import {Track} from "../models/Track";
+import pkceChallenge from "pkce-challenge";
 import SearchForItemParameterObject = SpotifyApi.SearchForItemParameterObject;
 import AlbumSearchResponse = SpotifyApi.AlbumSearchResponse;
-import {AlbumSearch} from "../models/AlbumSearch";
 import AlbumTracksResponse = SpotifyApi.AlbumTracksResponse;
-import {Track} from "../models/Track";
 
 const tokenStorageKey = 'spotifyToken';
+const challengeStorageKey = 'challenge';
+const client_id = 'c8dc40c2295a4e40accf71adb171fbff';
 
-const spotifyInstance = axios.create({
+let spotifyInstance: AxiosInstance;
+spotifyInstance = axios.create({
     baseURL: 'https://api.spotify.com/v1/',
     headers: {
         Authorization: 'Bearer ' + sessionStorage.getItem(tokenStorageKey)
     }
 });
 
-export async function authenticate() {
-    const client_id = 'c8dc40c2295a4e40accf71adb171fbff';
-    const client_secret = 'CHANGEME';
+export function login() {
+    const challenge = pkceChallenge();
+    localStorage.setItem(challengeStorageKey, challenge.code_verifier);
+    window.location.href = axios.getUri({
+        url: 'https://accounts.spotify.com/authorize', params: {
+            client_id,
+            response_type: 'code',
+            redirect_uri: 'http://localhost:3000',
+            code_challenge_method: "S256",
+            code_challenge: challenge.code_challenge
+        },
+    });
+}
 
-
+export async function getToken(code: string) {
     const data = new URLSearchParams();
-    data.append('grant_type', 'client_credentials')
+    data.append('grant_type', 'authorization_code')
+    data.append('code', code)
+    data.append('redirect_uri', 'http://localhost:3000');
+    data.append('client_id', client_id,);
+    data.append('code_verifier', localStorage.getItem(challengeStorageKey) as string);
 
     let response = await axios.post<any>('https://accounts.spotify.com/api/token', data, {
         headers: {
-            Authorization: 'Basic ' + btoa(client_id + ':' + client_secret),
             'Content-Type': 'application/x-www-form-urlencoded'
         },
     });
-
     sessionStorage.setItem(tokenStorageKey, response.data.access_token);
 }
+
 
 export async function fetchAlbums(search: string): Promise<AlbumSearch[]> {
     const queryParams: SearchForItemParameterObject = {
@@ -53,7 +70,7 @@ export async function fetchAlbums(search: string): Promise<AlbumSearch[]> {
 }
 
 export async function fetchAlbumTracks(albumId: string): Promise<Track[]> {
-    const req = await spotifyInstance.get<AlbumTracksResponse>('https://api.spotify.com/v1/albums/' + albumId + '/tracks',)
+    const req = await spotifyInstance.get<AlbumTracksResponse>('albums/' + albumId + '/tracks',)
     return req.data.items.map(track => {
         return {
             id: track.id, name: track.name, trackNumber: track.track_number
